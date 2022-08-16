dcc6026bfc8f8b57877a8d63420df0d01a0e1e42
Karen Ho <49111213+karengineering@users.noreply.github.com>
2022-08-16 12:31:22 -0700

    work on coursedetail...problem with desc

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 import axios from 'axios';
 
 //stateful component
@@ -24,13 +25,48 @@ export default function CourseDetail() {
             });
     }, [id]);
 
+    // console.log(course.user.firstName);
+
     //     console.log('a')
     // }, []);
 
 
     //course-detail.html
     return (
+        <main>
+            <div className="actions--bar">
+                <div className="wrap">
+                    <React.Fragment>
+                        <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+                        <Link className="button" to="#">Delete Course</Link>
+                        <Link className="button button-secondary" to="/">Return to List</Link>
+                    </React.Fragment>
+                </div>
+            </div>
 
-        <h1>Hello {id}</h1>
+            <div className="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div className="main--flex">
+                        <div>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{course.title}</h4>
+                            <p>By {course.user.firstName} {course.user.lastName}</p>
+                            <ReactMarkdown>{course.description}</ReactMarkdown>                         
+                        </div>
+                        <div>
+                            <h3 className="course--detail--title">Estimated Time</h3>
+                            <p>{course.estimatedTime}</p>
+
+                            <h3 className="course--detail--title">Materials Needed</h3>
+                            <ul className="course--detail--list">
+                                <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>
     );
+    // return (<h1>Hello</h1>);
 }
